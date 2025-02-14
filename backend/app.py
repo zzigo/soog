d39f4190c9e9ee97165cde9ba94da85b44381ed7
luciano azzigotti <lucianoazzigotti@gmail.com>
2025-02-14 13:30:12 +0100

    added scipy

--- backend/app.py
+++ backend/app.py
@@ -14,6 +14,7 @@ import logging
 import re
 import sys
 import torch
+import scipy
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel, AutoTokenizer, AutoModel
 from torch import nn
